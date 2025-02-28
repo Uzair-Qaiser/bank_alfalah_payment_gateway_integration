11d56d204fd3b408dd5b6229bcbfc0b3ec1f99c2
Uzair-Qaiser <iamuzairqaiser@gmail.com>
2025-03-01 04:09:15 +0500

    Initial commit

---
+++ lib/alfalah_payment.dart
@@ -0,0 +1,7 @@
+library alfalah_payment;
+
+export 'src/ui/alfalah_payment_integration_view.dart';
+export 'src/ui/post_payment_screen.dart';
+export 'src/service/payment_service.dart';
+export 'src/models/payment_model.dart';
+export 'src/utils/costants.dart';
---
+++ test/bank_alfalah_payment_gateway_integration_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bank_alfalah_payment_gateway_integration/alfalah_payment.dart';
+
+// void main() {
+//   test('PaymentService should return success', () {
+//     final paymentService = PaymentService();
+//
+//     final result = paymentService.processPayment(1000);
+//
+//     expect(result, true);  // Assuming it returns true on success
+//   });
+// }
